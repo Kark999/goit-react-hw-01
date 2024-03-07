c8cf2f1ee3c4fc267cc6e3fa9ac0dad6043568b5
Andrey Svyrskyi <akkad1v@gmail.com>
2024-03-07 07:49:12 +0200

    'update'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,18 @@
-// import userData from '../userData.json';
 // import friends from '../friends.json';
 // import transactions from '../transactions.json';
 // import profile from '../components/Profile';
 // import friendList from '../components/FriendList';
 // import transactionHistory from '../components/TransactionHistory';
-
 import './App.css';
+import Profile from './Profile';
+import ProfileData from '../components/Profile.json';
+
+const App = () => {
+  return (
+    <div>
+      <Profile {...ProfileData} />;
+    </div>
+  );
+};
+
+export default App;
