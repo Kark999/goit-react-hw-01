b80dedc646ecebc6e57e6d777d60c845f1511ba6
Andrey Svyrskyi <akkad1v@gmail.com>
2024-03-07 04:29:48 +0200

    'update'

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+// import userData from '../userData.json';
+// import friends from '../friends.json';
+// import transactions from '../transactions.json';
+// import profile from '../components/Profile';
+// import friendList from '../components/FriendList';
+// import transactionHistory from '../components/TransactionHistory';
+
+import './App.css';
