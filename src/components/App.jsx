5e98cfde34dcbdce260d47c0fe01cb1bef557b81
Andrey Svyrskyi <akkad1v@gmail.com>
2024-03-08 21:56:12 +0200

    'update'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Profile from '../components/Profile/Profile';
 import userData from '../assets/Profile.json';
 import FriendList from '../components/FriendList/FriendList';
 import friendsData from '../assets/FriendList.json';
+import TransactionHistory from '../components/TransactionHistory/TransactionHistory';
+import transactions from '../assets/TransactionHistory.json';
 
 const App = () => {
   const { username, tag, location, avatar, stats } = userData;
@@ -17,6 +19,7 @@ const App = () => {
         stats={stats}
       />
       <FriendList friends={friendsData} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
