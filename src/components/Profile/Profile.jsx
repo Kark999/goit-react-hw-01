d06ba6fc50dc5b9f0fe73b20e1b09d898a3fcad8
Andrey Svyrskyi <akkad1v@gmail.com>
2024-03-09 20:42:42 +0200

    'update'

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,27 @@
+import css from './Profile.module.css';
+
 const Profile = ({ name, image, tag, location, stats }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.profileBox}>
+      <div className={css.profileInfo}>
+        <img className={css.profileImage} src={image} alt="User avatar" />
+        <p className={css.profileName}>{name}</p>
+        <p className={css.profileTag}>@{tag}</p>
+        <p className={css.profileLocation}>{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.profileStats}>
+        <li className={css.profileActive}>
+          <span className={css.profileActiveName}>Followers</span>
+          <span className={css.profileActiveValue}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.profileActive}>
+          <span className={css.profileActiveName}>Views</span>
+          <span className={css.profileActiveValue}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.profileActive}>
+          <span className={css.profileActiveName}>Likes</span>
+          <span className={css.profileActiveValue}>{stats.likes}</span>
         </li>
       </ul>
     </div>
